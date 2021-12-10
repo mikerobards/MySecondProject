6cb9d0b24dd72fcfcfbe847e4b8e3455cd7d3090
Michael Robards <mikerobards@icloud.com>
2021-12-10 14:59:27 -0500

    Add practice

---
+++ GreetingAppPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+Console.WriteLine("Hello User! Welcome to the greeting app!");
+Console.WriteLine("This application was built by Mike Robards 2021");
+Console.WriteLine("-----------------------------------------------");
+Console.WriteLine();
+
+Console.Write("What is your first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine();
+
+Console.Write("What is your last name?");
+string lastName = Console.ReadLine();
+Console.WriteLine();
+
+Console.WriteLine($"Hello {firstName} {lastName}!");
+Console.WriteLine();
+
+Console.WriteLine("This application is complete. Good bye!");
+
+Console.ReadLine();

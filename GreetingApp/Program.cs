ebdd814478ba3c864d3fe891d9c0f69df70bdee2
Michael Robards <mikerobards@icloud.com>
2021-12-10 14:24:06 -0500

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+// Welcome user to app
+Console.WriteLine("Hello User to the Greeting App!");
+Console.WriteLine("This application was built by Mike Robards");
+Console.WriteLine("------------------------------------------");
+Console.WriteLine();
+
+// Ask user for first name
+Console.Write("What is your first name? ");
+string firstName = Console.ReadLine();
+
+// Greet user by first name
+Console.WriteLine();
+Console.WriteLine($"Hello {firstName}!");
+
+Console.WriteLine("Thank you for using my application!");
+
+Console.ReadLine();
+ 
+

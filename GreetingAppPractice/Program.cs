6f2d089f0b9f6d2bdb4e7d86ff538656db9d5ca3
Michael Robards <mikerobards@icloud.com>
2021-12-10 15:00:53 -0500

    edit text

--- GreetingAppPractice/Program.cs
+++ GreetingAppPractice/Program.cs
@@ -6,15 +6,15 @@ Console.WriteLine("This application was built by Mike Robards 2021");
 Console.WriteLine("-----------------------------------------------");
 Console.WriteLine();
 
-Console.Write("What is your first name?");
+Console.Write("What is your first name? ");
 string firstName = Console.ReadLine();
 Console.WriteLine();
 
-Console.Write("What is your last name?");
+Console.Write("What is your last name? ");
 string lastName = Console.ReadLine();
 Console.WriteLine();
 
-Console.WriteLine($"Hello {firstName} {lastName}!");
+Console.WriteLine($"Hello, {firstName} {lastName}!");
 Console.WriteLine();
 
 Console.WriteLine("This application is complete. Good bye!");
